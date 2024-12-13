7a1cebeec8cb9b2fcfcd2ed9dd990b3dce2fc82a
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-12-14 04:31:04 +0530

    Updated Files

--- Assignment 1st/assignQ6.py
+++ Assignment 1st/assignQ6.py
@@ -1,5 +1,6 @@
 # Q.6 find factorial & check palindrome numbers?
 
+from recursiveFuncModule import factorial,factorial_recursive,is_palindrome
 num = int(input("Enter Number you want to check : "))
 
 ##### Factorial Number #####
@@ -22,3 +23,20 @@ if original_num == rev_num : # compare the original string and reversed string
   print(f"{num} is a Palindrome number.")
 else :
   print(f"{num} is not a Palindrome number.")
+
+
+##### Factorial Calculation #####
+# Using the iterative `factorial` function
+iterative_fact = factorial(num)
+print(f"The factorial of {num} (Iterative) is: {iterative_fact}")
+
+# Using the recursive `factorial_recursive` function
+recursive_fact = factorial_recursive(num)
+print(f"The factorial of {num} (Recursive) is: {recursive_fact}")
+
+##### Palindrome Check #####
+# Using the `is_palindrome` function
+if is_palindrome(num):
+    print(f"{num} is a Palindrome number.")
+else:
+    print(f"{num} is not a Palindrome number.")
