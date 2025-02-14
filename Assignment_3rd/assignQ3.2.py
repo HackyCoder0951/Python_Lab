05e7da077934f3d3d9fe670066f5e95b91d40e8a
hackycoder0951 <jignesh.vision.17@gmail.com>
2025-02-14 23:56:52 +0530

    assignQ3.2.py Update

--- Assignment_3rd/assignQ3.2.py
+++ Assignment_3rd/assignQ3.2.py
@@ -3,11 +3,11 @@
 # except the numbers or digits in the file.
 
 def file_Written():
-    with open("python.txt","r") as file2, open("python1.txt","w") as file3:
+    with open("Assignment_3rd/python.txt","r") as file2, open("Assignment_3rd/python1.txt","w") as file3:
             for i in file2:
                 for char in i:
                     if not char.isnumeric():
                         file3.write(char)
 
-with open("python1.txt", "r") as file:
+with open("Assignment_3rd/python1.txt", "r") as file:
     print(file.read())
